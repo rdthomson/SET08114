36f696ce7c11935eaed787b7554d603e68870cfb
Ryan Thomson <rthomson97@me.com>
2017-10-15 21:17:27 +0100

    Initial upload of Coursework source code

---
+++ app/src/main/java/uk/ac/napier/mobileapplicationscoursework/fragments/DrinksFragment.java
@@ -0,0 +1,23 @@
+package uk.ac.napier.mobileapplicationscoursework.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import uk.ac.napier.mobileapplicationscoursework.R;
+
+/**
+ * Created by ryanthomson on 03/04/2016.
+ */
+public class DrinksFragment extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View rootview = inflater.inflate(R.layout.fragment_drinks, container,false);
+
+        return rootview;
+    }
+
+}
